e409ab2beb33f009a8c31981a8378dbea8da442f
Adibhange <bhangeaditya13@gmail.com>
2024-12-18 10:56:47 +0530

    Add icon and logo file with SVG icons

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,jsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(26, 26, 26)",
+        foreground: "rgb(38, 38, 38)",
+        border: "rgb(64, 64, 64)",
+
+        copy: "rgb(251, 251, 251)",
+        "copy-light": "rgb(217, 217, 217)",
+        "copy-lighter": "rgb(166, 166, 166)",
+
+        success: "rgb(0, 255, 0)",
+        warning: "rgb(255, 255, 0)",
+        error: "rgb(255, 0, 0)",
+
+        "success-content": "rgb(0, 0, 0)",
+        "warning-content": "rgb(0, 0, 0)",
+        "error-content": "rgb(255, 255, 255)",
+      },
+    },
+  },
+  plugins: [],
 };
