f2b8f08ddc5bc9fa6fe37613c7b393a043610a34
Adibhange <bhangeaditya13@gmail.com>
2024-12-26 12:40:06 +0530

    Connect backend API to sign up and sign in user

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,40 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "./../../axios.config";
+import { useDispatch, useSelector } from "react-redux";
+import { requestStart, signInSuccess } from "../redux/slices/userSlice";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useSelector((state) => state.user.user);
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(requestStart());
+      const res = await axios.post(
+        `${import.meta.env.VITE_REACT_APP_BASE_URL}/user/sign-in`,
+        {
+          email,
+          password,
+        },
+      );
+
+      const user = await res.data;
+      if (user) {
+        dispatch(signInSuccess(user));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.response?.data?.message || "Sign in failed");
+    }
+  };
+
   return (
     <section className="container mx-auto my-4 w-1/2 space-y-4 rounded-xl bg-foreground p-4">
       <h1 className="text-center text-3xl font-bold">Sign In</h1>
@@ -14,6 +48,7 @@ const SignIn = () => {
             name="email"
             placeholder="Enter your email"
             className="rounded-lg bg-background p-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -24,12 +59,14 @@ const SignIn = () => {
             name="password"
             placeholder="Enter your password"
             className="rounded-lg bg-background p-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
         <button
           type="submit"
           className="w-full rounded-lg bg-[#ff3533] p-2 text-white transition-colors duration-300 hover:bg-[#ff0200]"
+          onClick={handleSignIn}
         >
           Sign in
         </button>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,97 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { app } from "./../utils/firebase";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import axios from "./../../axios.config";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [userAvatar, setUserAvatar] = useState(null);
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (userAvatar.size > 2 * 1024 * 1024) {
+      setError("User Avatar must be less than 2MB.");
+      return;
+    }
+
+    try {
+      // Upload userAvatar to Firebase Storage
+      const storage = getStorage(app);
+      const userAvatarName = `user/${userName} - ${Date.now()}`;
+      const storageRef = ref(storage, userAvatarName);
+      const uploadTask = uploadBytesResumable(storageRef, userAvatar);
+
+      // Monitor upload progress
+      uploadTask.on("state_changed", (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        );
+      });
+
+      await new Promise((resolve, reject) => {
+        uploadTask.on(
+          "state_changed",
+          null,
+          (error) => reject(error),
+          () => resolve(),
+        );
+      });
+
+      const userAvatarURL = await getDownloadURL(uploadTask.snapshot.ref);
+
+      const res = await axios.post(
+        `${import.meta.env.VITE_REACT_APP_BASE_URL}/user/sign-up`,
+        {
+          userName,
+          email,
+          password,
+          confirmPassword,
+          userAvatar: userAvatarURL,
+        },
+      );
+      const newUser = res.data;
+      if (newUser) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setError(error.response?.data?.message || "Sign up failed.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <section className="container mx-auto my-4 w-1/2 space-y-4 rounded-xl bg-foreground p-4">
       <h1 className="text-center text-3xl font-bold">Sign Up</h1>
 
       <form className="mx-auto w-1/2 space-y-6">
+        {/* Avatar */}
+        <div className="flex flex-col gap-2">
+          <label className="text-xl">Profile Image:</label>
+          <input
+            type="file"
+            id="userAvatar"
+            name="userAvatar"
+            accept="image/*"
+            className="rounded-lg bg-background p-2 file:rounded-md file:border-0 file:bg-foreground file:px-4 file:py-2 file:text-sm file:font-medium file:text-copy focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setUserAvatar(e.target.files[0])}
+          />
+        </div>
+
         <div className="flex flex-col gap-2">
           <label className="text-xl">Username:</label>
           <input
@@ -14,6 +100,7 @@ const SignUp = () => {
             name="userName"
             placeholder="Enter your username"
             className="rounded-lg bg-background p-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setUserName(e.target.value)}
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -24,6 +111,7 @@ const SignUp = () => {
             name="email"
             placeholder="Enter your email"
             className="rounded-lg bg-background p-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -34,6 +122,7 @@ const SignUp = () => {
             name="password"
             placeholder="Enter your password"
             className="rounded-lg bg-background p-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -44,13 +133,16 @@ const SignUp = () => {
             name="confirmPassword"
             placeholder="Confirm your password"
             className="rounded-lg bg-background p-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </div>
         <button
           type="submit"
           className="w-full rounded-lg bg-[#ff3533] p-2 text-white transition-colors duration-300 hover:bg-[#ff0200]"
+          onClick={handleSignUp}
+          disabled={isLoading}
         >
-          Sign in
+          {isLoading ? "Signing Up..." : "Sign Up"}
         </button>
       </form>
 
